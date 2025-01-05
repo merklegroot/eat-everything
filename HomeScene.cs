27669e111d31f421c846c5a736ad9d61fe8e8799
Merkle Groot <merklegroot@gmail.com>
2025-01-04 23:24:31 -0500

    detecting tiles

--- HomeScene.cs
+++ HomeScene.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 namespace EatEverything;
 
@@ -12,6 +13,15 @@ public partial class HomeScene : Node2D
 
 	private Vector2 _originalPosition;
 
+	private TileMapLayer _tileMap;
+	
+	// You can customize this class based on what tile data you need
+	private class TileData
+	{
+		public Vector2I Position { get; set; }
+		public int TileId { get; set; }
+		public Vector2I AtlasCoords { get; set; }
+	}
 
 	private static class Animations
 	{
@@ -27,6 +37,15 @@ public partial class HomeScene : Node2D
 		_knight.Play(Animations.Horizontal);
 
 		_originalPosition = new Vector2(_characterBody.Position.X, _characterBody.Position.Y);
+
+		_tileMap = GetNode<TileMapLayer>("TileMapLayer");
+		var tiles = GetAllTiles();
+		
+		// Debug print to see what we found
+		foreach (var tile in tiles)
+		{
+			GD.Print($"Tile at {tile.Position}, ID: {tile.TileId}, Atlas: {tile.AtlasCoords}");
+		}
 	}
 
 	private bool _wasJumpReleased = true;
@@ -95,5 +114,35 @@ public partial class HomeScene : Node2D
 		_characterBody.Position = new Vector2(_originalPosition.X, _originalPosition.Y);
 		_wasJumpReleased = true;
 		_jumpStartTime = null;
+	}
+
+	private List<TileData> GetAllTiles()
+	{
+		var tiles = new List<TileData>();
+		
+		// Get the used rect of the tilemap (area containing tiles)
+		var usedRect = _tileMap.GetUsedRect();
+		
+		// Iterate through all cells in the rect
+		for (int x = usedRect.Position.X; x < usedRect.Position.X + usedRect.Size.X; x++)
+		{
+			for (int y = usedRect.Position.Y; y < usedRect.Position.Y + usedRect.Size.Y; y++)
+			{
+				var pos = new Vector2I(x, y);
+				
+				// Check if there's a tile at this position
+				if (_tileMap.GetCellSourceId(pos) != -1)  // 0 is the layer number
+				{
+					tiles.Add(new TileData
+					{
+						Position = pos,
+						TileId = _tileMap.GetCellSourceId(pos),
+						AtlasCoords = _tileMap.GetCellAtlasCoords(pos)
+					});
+				}
+			}
+		}
+
+		return tiles;
 	}
 }
